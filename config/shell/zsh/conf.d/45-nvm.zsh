7eb42cc4bdb45bad329e9e7729c4ef9c417317cc
TJ <mosesjames2000@gmail.com>
2026-03-14 18:29:54 -0400

    removed nvm

--- config/shell/zsh/conf.d/45-nvm.zsh
+++
@@ -1,7 +0,0 @@
-# NVM (Node Version Manager)
-export NVM_DIR="$HOME/.nvm"
-[[ -s "$NVM_DIR/nvm.sh" ]] && source "$NVM_DIR/nvm.sh"
-
-if command -v nvm >/dev/null 2>&1; then
-  nvm use --silent default >/dev/null 2>&1 || true
-fi
